87735a3e81a4b1d7b59e0da1e1e6aa078d71d326
sjtommylee <sjtommylee@gmail.com>
2023-03-22 11:20:23 -0700

    chore: commit save point

--- scanner.py
+++ scanner.py
@@ -14,11 +14,14 @@ response = input("""\n Enter the type of scan
             1)SYNC ACK Scan
             2)UDP San
             3)Comprehensive Scan
-""")
+\n""")
 
 print("selected option: ", response)
 
 if response == '1':
     print("nmap version: ", scanner.nmap_version())
-    scanner.scan(ip_address, '1-1024', -'v -s5')
+    scanner.scan(ip_address, '1-1024', '-v -sS')
     print(scanner.scaninfo())
+    print("ip status: ", scanner[ip_address].state())
+    print(scanner[ip_address].all_protocols())
+    print("open ports: ", scanner[ip_address]['tcp'].keys())
