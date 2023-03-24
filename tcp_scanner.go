282542757b4eed2b5c464c25de34b7dbc7685f0d
sjtommylee <sjtommylee@gmail.com>
2023-03-23 18:10:08 -0700

    chore: commit save point

--- tcp_scanner.go
+++ tcp_scanner.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"net"
 	"sort"
+	"sync"
 	"time"
 )
 
 type Silent struct{}
+
+func validateIPAddress(targetIP string) error {
+	ip := net.ParseIP(targetIP)
+	if ip == nil {
+		return errors.New("invalid IP address")
+	}
+	return nil
+}
 
 // defining a worker, taking two channels of int ports and results as params
 func worker(ports, results chan int, target string) {
@@ -25,38 +35,105 @@ func worker(ports, results chan int, target string) {
 	}
 }
 
+// func main() {
+// 	startTime := time.Now()
+// 	urlArg := flag.String("url", "", "source url")
+// 	flag.Parse()
+
+// 	// initalize ports and results and empty slice called openports
+// 	ports := make(chan int, 100)
+// 	results := make(chan int)
+// 	var openports []int
+
+// 	if *urlArg == "" {
+// 		fmt.Println("Please specify a target URL using the --url flag")
+// 		return
+// 	}
+// 	target := *urlArg
+
+// 	// creates worker goroutines for scanning ports. cap(ports) returns the capacity of the ports (100), goroutine reads from the ports and writes to results channel
+// 	for i := 0; i < cap(ports); i++ {
+// 		go worker(ports, results, target)
+// 	}
+
+// 	// go routine that sends each port number in the given range to the ports channel
+// 	// @goroutine - ltes us scan multiple ports simultaenously. in this case, we can scan 100 ports concurrently
+// 	go func() {
+// 		for i := 1; i <= 1024; i++ {
+// 			ports <- i
+// 			// fmt.Printf("Sent port %d to worker\n", i)
+// 		}
+// 	}()
+
+// 	// we then collect the results from the results channel, and appends the open ports to the slice defined
+
+// 	for port := range results {
+// 		if port != 0 {
+// 			openports = append(openports, port)
+// 			fmt.Printf("Port %d is open\n", port)
+// 		} else {
+// 			// fmt.Printf("Port %d is closed\n", port)
+// 		}
+// 	}
+
+// 	close(ports)
+// 	close(results)
+// 	sort.Ints(openports)
+// 	fmt.Println("Open ports:")
+// 	for _, port := range openports { // finallt we want to sort and print the port numbers
+// 		fmt.Printf("%d open\n", port)
+// 	}
+
+// 	endTime := time.Now()
+// 	elapsedTime := endTime.Sub(startTime)
+// 	fmt.Printf("Elapsed time: %v\n", elapsedTime)
+// }
+
 func main() {
 	startTime := time.Now()
-	urlArg := flag.String("url", "", "source url")
+	targetIP := flag.String("ip", "", "taret ip")
 	flag.Parse()
 
-	// initalize ports and results and empty slice called openports
 	ports := make(chan int, 100)
 	results := make(chan int)
 	var openports []int
 
-	if *urlArg == "" {
-		fmt.Println("Please specify a target URL using the --url flag")
+	if *targetIP == "" {
+		fmt.Println("Please specify a target IP address using the --targetIP flag")
 		return
 	}
-	target := *urlArg
+	if err := validateIPAddress(*targetIP); err != nil {
+		fmt.Println(err)
+		return
+	}
+	target := *targetIP
 
-	// creates worker goroutines for scanning ports. cap(ports) returns the capacity of the ports (100), goroutine reads from the ports and writes to results channel
+	// Create a wait group to wait for all the workers to finish
+	var wg sync.WaitGroup
+	wg.Add(cap(ports))
+
+	// Start the worker goroutines
 	for i := 0; i < cap(ports); i++ {
-		go worker(ports, results, target)
+		go func() {
+			worker(ports, results, target)
+			wg.Done()
+		}()
 	}
 
-	// go routine that sends each port number in the given range to the ports channel
-	// @goroutine - ltes us scan multiple ports simultaenously. in this case, we can scan 100 ports concurrently
+	// Send the port numbers to the ports channel
 	go func() {
 		for i := 1; i <= 1024; i++ {
 			ports <- i
-			// fmt.Printf("Sent port %d to worker\n", i)
 		}
 	}()
 
-	// we then collect the results from the results channel, and appends the open ports to the slice defined
+	// Close the results channel after all the workers are done
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
 
+	// Collect the results
 	for port := range results {
 		if port != 0 {
 			openports = append(openports, port)
@@ -66,11 +143,11 @@ func main() {
 		}
 	}
 
+	// Sort and print the open ports
 	close(ports)
-	close(results)
 	sort.Ints(openports)
 	fmt.Println("Open ports:")
-	for _, port := range openports { // finallt we want to sort and print the port numbers
+	for _, port := range openports {
 		fmt.Printf("%d open\n", port)
 	}
 
