52b389ba5dd875cbc31d9662459bb575eea3f6a0
sjtommylee <sjtommylee@gmail.com>
2023-03-23 17:33:08 -0700

    chore: commit save point

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"sort"
+)
+
+// defining a worker, taking two channels of int ports and results as params
+func worker(ports, results chan int) {
+	for p := range ports { // for loop to iterate through the ports channel until closed.
+		address := fmt.Sprintf("scanme.nmap.org:%d", p) // generates string representation of the remote host and the port being scanned. 'p' is the current port
+		conn, err := net.Dial("tcp", address)           // with the given address, attempt to open a TCP conneection.
+		if err != nil {
+			results <- 0 // check to see if the conn is successful. if there is an error, close with a value of 0 which is sent to the results channel.
+			continue     // skip to the next iteration of the loop
+		}
+		conn.Close() // when we do have a successful connection, we close it
+		results <- p // also when we are successful, send the value of p (current port being scanned) to the results
+	}
+}
+
+func main() {
+	// initalize ports and results and empty slice called openports
+	ports := make(chan int, 100)
+	results := make(chan int)
+	var openports []int
+
+	// creates worker goroutines for scanning ports. cap(ports) returns the capacity of the ports (100), goroutine reads from the ports and writes to results channel
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, results)
+	}
+
+	// go routine that sends each port number in the given range to the ports channel
+	// @goroutine - ltes us scan multiple ports simultaenously. in this case, we can scan 100 ports concurrently
+	go func() {
+		for i := 1; i <= 1024; i++ {
+			ports <- i
+		}
+	}()
+
+	for i := 0; i < 1024; i++ {
+		port := <-results
+		if port != 0 {
+			openports = append(openports, port)
+		}
+	}
+
+	close(ports)
+	close(results)
+	sort.Ints(openports)
+	for _, port := range openports {
+		fmt.Printf("%d open\n", port)
+	}
+}
